ed22bc9920fd828f7bbc5abf4fde7447c52ed6c9
iamAbasu7 <antar.basu@zeeve.io>
2022-06-30 15:53:48 +0530

    from to for

--- js/transaction.js
+++ js/transaction.js
@@ -172,12 +172,12 @@ function generateTxInfo(result, network) {
 
                             if (log.topics[1])
                                 //ffrom = '<a class="address-tag" href="https://' + url + '/address/' + convertHex2Addr(log.topics[1]) + '" rel="nofollow">' + convertHex2Addr(log.topics[1]) + '</a>';
-                                ffrom =  fromAddress /n;
+                                ffrom =  fromAddress.write("\n");
                             if (log.topics[2])
                                 //tto = '<a class="address-tag" href="https://' + url + '/address/' + convertHex2Addr(log.topics[2]) + '" rel="nofollow">' + convertHex2Addr(log.topics[2]) + '</a>';
                                 tto = toAddress ;
                             if (log.topics[1] && log.topics[2]) {
-                                tokenTransfer += 'From ' + ffrom  + ' To: ' + tto + ' for: ';
+                                tokenTransfer += 'From: ' + ffrom  + ' To: ' + tto + ' for: ';
 
                                 if (log.topics[3]){
                                     tokenTransfer += new BigNumber(log.topics[3]).toString();
